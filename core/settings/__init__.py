bc44c2f35a193744ce9d0c98a559a8e17d6ffda7
vlauncher <v2dent@gmail.com>
2024-11-10 15:38:05 +0100

    New Commit

--- core/settings/__init__.py
+++
@@ -1,6 +0,0 @@
-from .base import *
-
-if environ.get('DEBUG') == 'True':
-    from .development import *
-else:
-    from .production import *
--- portfolio/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('resume/', views.resume, name='resume'),
-    path('projects/', views.projects, name='projects'),
-    path('contact/', views.contacts, name='contacts'),
-]
